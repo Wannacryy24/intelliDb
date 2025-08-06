9cb4d338465af280b4c2ad97d102ffacc69b5fd0
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-08-06 14:15:30 +0530

    fix error

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const BASE_URL = 'http://44.202.108.149:8000'; 
 // const BASE_URL = '/api';
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL; 
 
 // Connection
 export const connectToDb = (credentials) =>
